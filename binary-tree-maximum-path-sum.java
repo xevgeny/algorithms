c8188eb58e292ef3b955c5f57737fedf1b79b1f4
Evgeny <e.tskhovrebov@pm.me>
2019-08-15 16:15:33 +0200

    add comments

--- binary-tree-maximum-path-sum.java
+++ binary-tree-maximum-path-sum.java
@@ -9,8 +9,12 @@
  */
 
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/
-// For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections.
+//
+// For this problem, a path is defined as any sequence of nodes 
+// from some starting node to any node in the tree along the parent-child connections.
 // The path must contain at least one node and does not need to go through the root.
+//
+// e.g arbitrary path from one node to antoher
 
 class Solution {
     private int maxVal = Integer.MIN_VALUE;
@@ -22,9 +26,9 @@ class Solution {
     
     private int max(TreeNode node) {
         if (node == null) return 0;
-        int left = Math.max(0, max(node.left));
-        int right = Math.max(0, max(node.right));
+        int left = Math.max(0, max(node.left));   // does the left path contributes positively
+        int right = Math.max(0, max(node.right)); // same for the right path
         maxVal = Math.max(maxVal, node.val + left + right);
-        return node.val + Math.max(left, right);
+        return node.val + Math.max(left, right);  // choosing either left or right or none 
     }
 }
